4ef3461a12cdaddb918dc3bf56332322f54d091b
d-levin <dennis.a.levin@gmail.com>
2018-09-02 18:23:04 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld class="p-10 bg-orange" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
